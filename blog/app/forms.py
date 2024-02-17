b78265ea4c47907ee190c07c644a825726b654a5
Muruxo <83302154+Muruxo@users.noreply.github.com>
2024-02-16 23:31:21 -0500

    formulario agregar

--- blog/app/forms.py
+++ blog/app/forms.py
@@ -1,8 +1,35 @@
 from django import forms
+from django.utils import timezone
 from django.forms import ModelForm
 
-from .models import Publicacion
+from .models import Empleo, Publicacion
 
+MODALIDAD_EMPLEO_CHOICES = ["Activado", "Desactivado", "Pendiente"]
+
+class EmpleoForm(ModelForm):
+    class Meta: 
+        model = Empleo
+        fields = '__all__'
+        labels = {
+            'nombre_empleo': 'Empleo', 
+            'descripcion_empleo' : 'Descripción',
+            'fecha_empleo': 'Fecha de Publicación',
+            'area_empleo': 'Area del Empleo',
+            'modalidad_empleo': 'Modalidad de Trabajo',
+            'tiempo_empleo': 'Tiempo de contrato',
+            'id_ciudad_fk': 'Ciudad',
+            
+        }
+        widgets = {
+            'nombre_empleo': forms.TextInput(attrs = {'class': 'form-control'}),
+            'descripcion_empleo': forms.Textarea(attrs = {'class': 'form-control'}),
+            'fecha_empleo':forms.DateInput(attrs={'class': 'form-control', 'display':'center' }),
+            'area_empleo': forms.TextInput(attrs = {'class': 'form-control'}),
+            'modalidad_empleo': forms.TextInput(attrs = {'class': 'form-control'}),
+            'tiempo_empleo':forms.TextInput(attrs= {'type': 'number', 'class': 'form-control'}),
+            'id_ciudad_fk':forms.TextInput(attrs = {'class': 'form-control'}),
+        }
+        
 class PublicacionForm(ModelForm):
     class Meta: 
         model = Publicacion
@@ -17,3 +44,5 @@ class PublicacionForm(ModelForm):
             'descripcion': forms.Textarea(attrs = {'class': 'form-control'}),
             'email': forms.EmailInput(attrs = {'class': 'form-control'}),
         }
+        
+       
--- blog/app/views.py
+++ blog/app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from .models import Publicacion
-from  .forms import PublicacionForm
+from .models import Publicacion, Empleo
+from  .forms import PublicacionForm, EmpleoForm
 
 def actualizar(request, publicacion_id): 
     publicacion = Publicacion.objects.get(pk = publicacion_id)
@@ -21,17 +21,17 @@ def eliminar(request, publicacion_id):
     
 def agregar(request): 
     if request.POST: 
-        form = PublicacionForm(request.POST)
+        form = EmpleoForm(request.POST)
         if form.is_valid():
             form.save()
         messages.success(request, 'Publicacion Añadida')
         return redirect(home)  
 
-    return render(request, 'app/agregar.html', {'form':PublicacionForm})
+    return render(request, 'app/agregar.html', {'form':EmpleoForm})
 
 def home(request): 
-    publicaciones = Publicacion.objects.all()
-    return render(request, 'app/home.html', {'publicaciones': publicaciones})
+    empleos = Empleo.objects.all()
+    return render(request, 'app/home.html', {'empleos': empleos})
 
 def index(request):
     publicaciones = Publicacion.objects.all()
